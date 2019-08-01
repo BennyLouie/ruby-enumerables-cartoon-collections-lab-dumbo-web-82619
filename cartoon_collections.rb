c752c5550ce11507430e02532910cab349cd4472
Benny Louie <bennylouie1412@gmail.com>
2019-08-01 19:19:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,9 @@
-def roll_call_dwarves
+def roll_call_dwarves(array)
   i = 0 
-  
+  while i < array.length do 
+    puts array[i]
+    i += 1 
+  end 
 end
 
 def summon_captain_planet# code an argument here
