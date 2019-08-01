626cce589b8ceacfd732b4067ab1a2f623df4d27
Benny Louie <bennylouie1412@gmail.com>
2019-08-01 19:41:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planateer)
-  planateer.map {|call| "#{planateer.capitalize}!"}
+  planateer.map {|call| "#{planateer.capitalize!}!"}
 end
 
 def long_planeteer_calls# code an argument here
